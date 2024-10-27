b2d506c45d3062a277e42707fada7a71203aded8
Dillon <dillontaylor2@gmail.com>
2024-10-27 17:10:06 -0400

    add JS file

--- static/loginandcreate.js
+++ static/loginandcreate.js
@@ -3,4 +3,5 @@ function rotate(){
     var img = document.getElementById("logo");
     Rotation = Rotation - 90;
     img.style.transform = "rotate("+Rotation.toString()+"deg)";
+    console.log("flip");
 }
