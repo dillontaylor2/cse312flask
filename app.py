112c4b1d9db95922114302f287581e0a2d0c340d
Dillon <dillontaylor2@gmail.com>
2024-10-27 16:49:04 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def returnIndex():
+    return render_template('index.html')
+@app.route('/login')
+def returnLogin():  # put application's code here
+    return render_template('login.html')
+
+@app.route('/register')
+def returnRegister():
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    app.run()
