b48ec30e514d3e1dbd45b09fd7f2f375471ad562
Darren Chiu <dtc52@drexel.edu>
2024-11-28 00:43:36 -0500

    did a bunch of modifications to attempt to get the frontend of websockets finished. Added the static folder containing css. [Not Finished]

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageOps
-from flask import Flask, render_template, send_from_directory, redirect, request, make_response, url_for
+from flask import Flask, render_template, send_from_directory, redirect, request, make_response, url_for, jsonify
 from pymongo import MongoClient
+from flask_socketio import SocketIO, emit, join_room, leave_room
 import hashlib
 import os
 import uuid
@@ -8,8 +9,10 @@ import uuid
 
 from werkzeug.utils import secure_filename
 from flask_cors import CORS
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__, template_folder="templates", static_folder="static")
 #CORS(app)
+# Initialize SocketIO and set the CORS option
+socketio = SocketIO(app, cors_allowed_origins="*")
 UPLOAD_FOLDER = 'images'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -24,6 +27,8 @@ else:
 
 database = MC["La_fromage"]
 user_data = database["users"]
+posts_data = database["posts"]
+
 moz_data = database["Mozeralla"]
 ched_data = database["Cheddar"]
 brie_data = database["Brie"]
@@ -82,13 +87,74 @@ def add_nosniff(response):
     response.headers["X-Content-Type-Options"] = "nosniff"
     return response
 
+active_users = {}
+
+@socketio.on('connect')
+def handle_connect():
+    authtoken = request.cookies.get('authtoken')
+    user = check_user(authtoken) if authtoken else None
+    if user == "None" or user is None:
+        emit('error', {'message': 'Unauthorized'}, broadcast=False)
+        return False
+    username = user.get("username", "Guest")
+    emit('user_connected', {"username": username}, broadcast=True)
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    authtoken = request.cookies.get('authtoken')
+    user = check_user(authtoken) if authtoken else None
+    if user != "None" and user is not None:
+        username = user.get("username", "Guest")
+        emit('user_disconnected', {"username": username}, broadcast=True)
+
+@socketio.on('chat_message')
+def handle_chat_message(data):
+    authtoken = request.cookies.get('authtoken')
+    user = check_user(authtoken) if authtoken else "Guest"
+    username = user.get("username", "Guest")
+    message = data.get("message", "")
+    if message:
+        emit('chat_message', {"username": username, "message": message}, broadcast=True)
 
 
+@socketio.on('new_post')
+def handle_new_post(data):
+    authtoken = request.cookies.get('authtoken')
+    user = check_user(authtoken) if authtoken else "Guest"
+    username = user.get("username", "Guest")
+    post_content = data.get("content", "")
+    if post_content:
+        post_id = str(uuid.uuid4())
+        posts_data.insert_one({"post_id": post_id, "content": post_content, "author": username})
+        emit('new_post', {"post_id": post_id, "author": username, "content": post_content}, broadcast=True)
+
+@socketio.on('join')
+def handle_join(data):
+    room = data.get('room', 'default')
+    authtoken = request.cookies.get('authtoken')
+    user = check_user(authtoken) if authtoken else "Guest"
+    username = user.get("username", "Guest")
+    join_room(room)
+    emit('status', {'message': f'{username} joined {room}'}, room=room)
+
+@socketio.on('leave')
+def handle_leave(data):
+    room = data.get('room', 'default')
+    authtoken = request.cookies.get('authtoken')
+    user = check_user(authtoken) if authtoken else "Guest"
+    username = user.get("username", "Guest")
+    leave_room(room)
+    emit('status', {'message': f'{username} left {room}'}, room=room)
+
+@app.route('/posts', methods=['GET'])
+def get_posts():
+    posts = list(posts_data.find({}, {"_id": 0}))
+    return jsonify(posts)
 
 @app.route("/")
 def serve_first():
     colorChange = changecolorgen
-    cheesebannerlink = url_for('static', filename='cheesebanner.jpg')
+    cheesebannerlink = url_for('static', filename='logo.png')
     if "authtoken" in request.cookies:
         authtoken = request.cookies.get("authtoken")
         user = check_user(authtoken)
@@ -316,4 +382,6 @@ def add_user_to_like():
         return redirect("/",code=302)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    # Changed block to run under socket
+    # app.run(debug=True, host="0.0.0.0", port=8080)
+    socketio.run(app, host="0.0.0.0", port=8080, debug=True, allow_unsafe_werkzeug=True)
